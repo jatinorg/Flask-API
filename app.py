89f9c1f3d30470c24f0700f9ad7ebd96ad1e4a3d
Jatin Allamsetty <you@example.com>
2024-08-02 10:43:53 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from urllib.parse import urlparse
 import re
 import csv
 import requests
-from io import StringIO
+from io import StringIO, BytesIO
+import json
 
 app = Flask(__name__)
 
@@ -47,14 +48,8 @@ def retrieve_scholar_info(query, max_results=10):
 
             title = clean_text(publication['bib']['title'])
             authors = [clean_text(author) for author in publication['bib']['author']]
-            main_author = authors[0]
+            main_author = authors[0] if authors else "N/A"
             year = publication['bib']['pub_year']
-
-            eprint_url = publication.get('eprint_url', '')
-            journal_homepage = ''
-            if eprint_url:
-                parsed_url = urlparse(eprint_url)
-                journal_homepage = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
             num_citations = publication['num_citations'] if 'num_citations' in publication else 0
             impact_factor = calculate_impact_factor(num_citations)
@@ -71,7 +66,6 @@ def retrieve_scholar_info(query, max_results=10):
                 "Authors": ', '.join(authors),
                 "Main Author": main_author,
                 "Year": year,
-                "Journal Homepage": journal_homepage,
                 "Impact Factor": impact_factor,
                 "Citation URL": citation_url,
                 "Number of Citations": num_citations,
@@ -105,27 +99,34 @@ def download_csv():
     if not results:
         return "No data to download", 400
 
+    try:
+        results = json.loads(results)
+    except json.JSONDecodeError as e:
+        return f"JSONDecodeError: {e}", 400
+
     # Prepare CSV
-    si = StringIO()
-    writer = csv.writer(si)
-    writer.writerow(["Title", "Authors", "Main Author", "Year", "Journal Homepage", "Impact Factor", "Citation URL", "Number of Citations", "DOI", "Journal Name"])
+    output = StringIO()
+    writer = csv.writer(output)
+    writer.writerow(["Title", "Authors", "Main Author", "Year", "Impact Factor", "Citation URL", "Number of Citations", "DOI", "Journal Name"])
     for result in results:
         writer.writerow([
             result['Title'],
             result['Authors'],
             result['Main Author'],
             result['Year'],
-            result['Journal Homepage'],
             result['Impact Factor'],
             result['Citation URL'],
             result['Number of Citations'],
             result['DOI'],
             result['Journal Name']
         ])
-    si.seek(0)
+    output.seek(0)
+    
+    # Convert to bytes
+    csv_bytes = BytesIO(output.getvalue().encode('utf-8'))
 
     return send_file(
-        si,
+        csv_bytes,
         mimetype='text/csv',
         as_attachment=True,
         attachment_filename='scholar_results.csv'
